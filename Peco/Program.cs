92f71739e38e111a8bd488ca23aa8fb90900e1cd
santino parra rocha <smpr2021@hotmail.com>
2021-09-01 19:26:08 -0500

    Agregar archivos de proyecto.

---
+++ Peco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Peco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola soy peco!");
+        }
+    }
+}
